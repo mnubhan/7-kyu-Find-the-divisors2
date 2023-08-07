16e5e795e60249e2c3720e8666af61870d8cc8f3
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-08-08 00:10:13 +0100

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,3 +1,9 @@
 def divisors(interger):
   ls=[x+1 for x in range(interger)  if interger % (x + 1) == 0 and interger != x+1 and x+1!=1]
   return f"{interger} is prime" if ls == [] else ls
+
+def divisors(num):
+    l = [a for a in range(2,num) if num%a == 0]
+    if len(l) == 0:
+        return str(num) + " is prime"
+    return l
